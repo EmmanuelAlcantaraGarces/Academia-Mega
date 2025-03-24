36defd09b0442ee36ea1d4a97a377cedef74b346
EmmanuelAlcantaraGarces <51168188+EmmanuelAlcantaraGarces@users.noreply.github.com>
2025-03-24 11:08:35 -0600

    Add files via upload
    Entregables sesion 1

---
+++ Modulo_1_Javascript_Testing/01_sesion/README.md
@@ -0,0 +1,10 @@
+Sesión 1: Introducción a JavaScript
+Fecha: 24/03/2025
+
+Objetivos de la Sesión
+Introducción a variables de JavaScript
+
+Temas Cubiertos
+Fundamentos de JavaScript
+
+Declaración de variables
---
+++ Modulo_1_Javascript_Testing/01_sesion/introduccion_javascript.js
@@ -0,0 +1,15 @@
+//alert("Hola a todos, bienvenidos.");
+
+//Formas de declarar variables
+var int = 12345;
+var texto = "Texto";
+var boolean = true;
+
+var nombre = "Emmanuel";
+var apellido = "Alcantara Garces";
+var nombreCompleto = `${nombre} ${apellido}`;
+var edad = 46;
+
+//Uso de variables y salida de mensajes
+console.log(`Hola, soy ${nombreCompleto} y tengo ${edad} anios.`);
+console.log("Hola, soy " + nombreCompleto + " y tengo " + edad + " anios.");
